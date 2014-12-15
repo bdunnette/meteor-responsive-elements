6fee81e46fdbdbfdf9247e501e721efa2e8a7651
Brian Dunnette <dunn0172@umn.edu>
2014-12-15 12:12:51 -0600

    cleanup

--- package.js
+++ package.js
@@ -2,17 +2,11 @@ Package.describe({
   name: 'bdunnette:responsive-elements',
   summary: ' /* Fill me in! */ ',
   version: '1.0.0',
-  git: 'https://github.com/bdunnette/meteor-responsive-elements.git'
+  git: 'https://github.com/bdunnette/meteor-responsive-elements'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.0.1');
   api.use('jquery');
-  api.addFiles('responsive-elements.js','client');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('bdunnette:responsive-elements');
-  api.addFiles('bdunnette:responsive-elements-tests.js');
+  api.addFiles('responsive-elements.js', 'client');
 });
